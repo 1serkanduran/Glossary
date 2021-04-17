e469032d20b990bd8dcb66a9ffe4c34532a15ac0
1serkanduran <serkndrn@gmail.com>
2021-04-18 00:54:00 +0300

    deneme1

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -14,8 +14,8 @@ namespace DataAccessLayer.Concrete
         public DbSet<Category> Categories { get; set; }
         public DbSet<Contact> Contacts { get; set; }
         public DbSet<Content> Contents { get; set; }
-        public DbSet<Heading> Headings { get; set; }
-        public DbSet<Writer> Writers { get; set; }
+        //public DbSet<Heading> Headings { get; set; }
+        //public DbSet<Writer> Writers { get; set; }
 
     }
 }
