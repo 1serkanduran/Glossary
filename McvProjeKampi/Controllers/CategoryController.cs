bb9d4751a8414a228ee53158224d61c71db3450a
1serkanduran <serkndrn@gmail.com>
2021-05-06 22:38:48 +0300

    Use of Validators

--- McvProjeKampi/Controllers/CategoryController.cs
+++ McvProjeKampi/Controllers/CategoryController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrate;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,26 +14,40 @@ namespace McvProjeKampi.Controllers
     public class CategoryController : Controller
     {
         // GET: Category
-        //CategoryManager cm = new CategoryManager();
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
         public ActionResult Index()
         {
             return View();
         }
-        //public ActionResult GetCategoryList()
-        //{
-        //    var categoryvalues = cm.GetAllBL();
-        //    return View(categoryvalues);
-        //}
+        public ActionResult GetCategoryList()
+        {
+            var categoryvalues = cm.GetList();
+            return View(categoryvalues);
+        }
         [HttpGet]
         public ActionResult AddCategory()
         {
             return View();
         }
-        //[HttpPost]
-        //public ActionResult AddCategory(Category p)
-        //{
-        //    cm.CategoryAddBL(p);
-        //    return RedirectToAction("GetCategoryList");
-        //}
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            //cm.CategoryAddBL(p);
+            CategoryValidator categoriValidator = new CategoryValidator();
+            ValidationResult results = categoriValidator.Validate(p);
+            if (results.IsValid)
+            {
+                cm.CategoryAdd(p);
+                return RedirectToAction("GetCategoryList");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
     }
 }
