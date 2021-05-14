e9a01ee699d9197dc30ca4abe6dc9a8a7b033bf0
1serkanduran <serkndrn@gmail.com>
2021-05-14 19:06:29 +0300

    Writer Crud

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator:AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adı boş geçilemez");
+            RuleFor(x => x.WriterSurName).NotEmpty().WithMessage("Yazar soyadı boş geçilemez");
+            RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Hakkında kısmı boş geçilemez");
+            RuleFor(x => x.WriterSurName).MinimumLength(3).WithMessage("En az 2 karakter olmalıdır");
+            RuleFor(x => x.WriterSurName).MaximumLength(20).WithMessage("En fazla 50 karakter");
+        }
+    }
+}
